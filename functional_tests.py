54066fd35bd96b703bdad1c57a1474d82a5184d6
Richard Paul <richardnpaul@gmail.com>
2016-09-06 20:19:28 +0100

    Adding an implicitly_wait to class setup method of 3 seconds. The test test_can_login_and_be_redirected_to_the_profile_page still fails as the default landing page title is incorrect as urls have not yet been set up

--- functional_tests.py
+++ functional_tests.py
@@ -5,6 +5,7 @@ class ProfileUserTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
