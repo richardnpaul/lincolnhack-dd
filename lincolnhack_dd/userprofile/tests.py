1b40bb1f0369b0f5b2e7187408c3535b307c9a94
Richard Paul <richardnpaul@gmail.com>
2016-09-06 20:31:21 +0100

    Creating a very basic test for the login_page view which will be the root landing page. This page will show the user's profile view or a login view depending on whether they are a recognised authenticated user. This test fails with an import error because we've not created the login_page view yet.

--- lincolnhack_dd/userprofile/tests.py
+++ lincolnhack_dd/userprofile/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
 
-# Create your tests here.
+from .views import login_page
+
+
+class LoginPageTest(TestCase):
+
+    def test_root_url_resolves_to_the_login_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, login_page)
