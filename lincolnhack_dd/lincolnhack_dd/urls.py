d1d712312e9a16328c543d3a75a9f0f0a4c43765
Richard Paul <richardnpaul@gmail.com>
2016-09-07 18:56:16 +0100

    Create login_page view and comment out the admin site url and add a new url for the login_page view

--- lincolnhack_dd/lincolnhack_dd/urls.py
+++ lincolnhack_dd/lincolnhack_dd/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from userprofile import views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+    url(r'^$', views.login_page, name='login'),
 ]
