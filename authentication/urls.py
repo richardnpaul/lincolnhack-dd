2b1216bb7f12684f0ddbe0fab3c0778b1e8f6a90
Richard Paul <richardnpaul@gmail.com>
2016-10-08 19:28:36 +0100

    Started adding the password reset functionality, not needed for this 24 hour hack

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
         {'template_name': 'logged_out.html'}, name='logout'),
     url(r'^logout-then-login/$', views.logout_then_login,
         name='logout_then_login'),
+    #url(r'^password-change/$', views.password_change,
+    #    {'template_name': 'password_change_form.html'},
+    #    name='password_change'),
+    #url(r'^password-change/done/$', views.password_change_done,
+    #    {'template_name': 'password_change_done.html'},
+    #    name='password_change_done'),
 ]
